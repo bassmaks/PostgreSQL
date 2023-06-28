330114f269ba9e3f34bc27ed8a0bced2dbed0d8e
Maks Borisov <124687428+bassmaks@users.noreply.github.com>
2023-06-28 17:06:40 +0300

    отредактировал задание и файлы

---
+++ src Day00/day00_ex06.sql
@@ -0,0 +1,12 @@
+SELECT (SELECT name
+        FROM person
+        WHERE person.id = person_order.person_id) AS name,
+       (SELECT CASE
+                   WHEN name = 'Denis' THEN TRUE
+                   ELSE FALSE
+                   END
+        FROM person
+        WHERE person.id = person_order.person_id) AS check_name
+FROM person_order
+WHERE (person_order.menu_id = 13 OR person_order.menu_id = 14 OR person_order.menu_id = 18)
+  AND person_order.order_date = '2022-01-07';
