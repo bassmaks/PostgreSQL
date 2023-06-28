472176c9a24c066eb41cbc7d0908ac580bd392f3
Maks Borisov <124687428+bassmaks@users.noreply.github.com>
2023-06-28 17:05:35 +0300

    обновление без лишних папок

--- src Day00/ex04/day00_ex04.sql
+++
@@ -1,3 +0,0 @@
-SELECT name || ' (age:' || age || ',gender:''' || gender || ''',address:''' || address || ''')' AS person_information
-FROM person
-ORDER BY person_information;
--- src Day00/ex05/day00_ex05.sql
+++
@@ -1,6 +0,0 @@
-SELECT (SELECT name
-        FROM person
-        WHERE person.id = person_order.person_id) AS name
-FROM person_order
-WHERE (person_order.menu_id = 13 OR person_order.menu_id = 14 OR person_order.menu_id = 18)
-  AND person_order.order_date = '2022-01-07';
